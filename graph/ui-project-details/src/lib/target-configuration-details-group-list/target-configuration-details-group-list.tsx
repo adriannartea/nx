3ebae44d61ce01ba8d405689aacf2f0d43b7279d
Jack Hsu <jack.hsu@gmail.com>
2024-06-14 09:41:41 -0400

    fix(graph): improve layout when target groups are empty or there are no groups

--- graph/ui-project-details/src/lib/target-configuration-details-group-list/target-configuration-details-group-list.tsx
+++ graph/ui-project-details/src/lib/target-configuration-details-group-list/target-configuration-details-group-list.tsx
@@ -5,6 +5,7 @@ import type { ProjectGraphProjectNode } from '@nx/devkit';
 import { TargetConfigurationDetailsListItem } from '../target-configuration-details-list-item/target-configuration-details-list-item';
 import { TargetConfigurationGroupContainer } from '../target-configuration-details-group-container/target-configuration-details-group-container';
 import { groupTargets } from '../utils/group-targets';
+import { useMemo } from 'react';
 
 export interface TargetConfigurationGroupListProps {
   project: ProjectGraphProjectNode;
@@ -26,35 +27,18 @@ export function TargetConfigurationGroupList({
   onViewInTaskGraph,
   className = '',
 }: TargetConfigurationGroupListProps) {
-  const targetsGroup = groupTargets(project);
+  const targetsGroup = useMemo(() => groupTargets(project), [project]);
+  const hasGroups = useMemo(() => {
+    for (const group of Object.entries(targetsGroup.groups)) {
+      if (group[1]?.length > 0) return true;
+    }
+    return false;
+  }, [targetsGroup]);
 
-  return (
-    <>
-      {Object.entries(targetsGroup.groups).map(([targetGroupName, targets]) => {
-        return (
-          <TargetConfigurationGroupContainer
-            targetGroupName={targetGroupName}
-            targetsNumber={targets.length}
-            key={targetGroupName}
-          >
-            <ul className={className}>
-              {targets.map((targetName) => (
-                <TargetConfigurationDetailsListItem
-                  project={project}
-                  sourceMap={sourceMap}
-                  variant={variant}
-                  onRunTarget={onRunTarget}
-                  onViewInTaskGraph={onViewInTaskGraph}
-                  targetName={targetName}
-                  collapsable={true}
-                  key={targetName}
-                />
-              ))}
-            </ul>
-          </TargetConfigurationGroupContainer>
-        );
-      })}
-      <ul className={`mt-8 p-2 ${className}`}>
+  if (!hasGroups) {
+    // If there are no target groups, then don't show nested groups
+    return (
+      <ul className={`mt-8 ${className}`}>
         {targetsGroup.targets.map((targetName) => {
           return (
             <TargetConfigurationDetailsListItem
@@ -70,6 +54,60 @@ export function TargetConfigurationGroupList({
           );
         })}
       </ul>
-    </>
-  );
+    );
+  } else {
+    // Otherwise, show all the groups first, and the ungrouped targets are under "Others"
+    return (
+      <>
+        {Object.entries(targetsGroup.groups).map(
+          ([targetGroupName, targets]) => {
+            return (
+              <TargetConfigurationGroupContainer
+                targetGroupName={targetGroupName}
+                targetsNumber={targets.length}
+                key={targetGroupName}
+              >
+                <ul className={className}>
+                  {targets.map((targetName) => (
+                    <TargetConfigurationDetailsListItem
+                      project={project}
+                      sourceMap={sourceMap}
+                      variant={variant}
+                      onRunTarget={onRunTarget}
+                      onViewInTaskGraph={onViewInTaskGraph}
+                      targetName={targetName}
+                      collapsable={true}
+                      key={targetName}
+                    />
+                  ))}
+                </ul>
+              </TargetConfigurationGroupContainer>
+            );
+          }
+        )}
+        <TargetConfigurationGroupContainer
+          targetGroupName="Others"
+          targetsNumber={targetsGroup.targets.length}
+          key="other"
+        >
+          <ul className={`p-2 ${className}`}>
+            {targetsGroup.targets.map((targetName) => {
+              return (
+                <TargetConfigurationDetailsListItem
+                  project={project}
+                  sourceMap={sourceMap}
+                  variant={variant}
+                  onRunTarget={onRunTarget}
+                  onViewInTaskGraph={onViewInTaskGraph}
+                  targetName={targetName}
+                  collapsable={true}
+                  key={targetName}
+                />
+              );
+            })}
+          </ul>
+        </TargetConfigurationGroupContainer>
+      </>
+    );
+  }
 }
